4bc9243d5bf2f179615da6612cf7d06605e3bb3c
patrick ouambo <patrickouambo@gmail.com>
2023-03-18 15:46:00 +0100

    filtered habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+}
 
 fs.createReadStream('./kepler_data.csv')
 	.pipe(
@@ -10,11 +19,13 @@ fs.createReadStream('./kepler_data.csv')
 			columns: true,
 		})
 	)
-	.on('data', (obj) => {
-		results.push(obj);
+	.on('data', (planet) => {
+		if (isHabitablePlanet(planet)) {
+			habitablePlanets.push(planet);
+		}
 	})
 	.on('error', (error) => console.log(error))
 	.on('end', () => {
-		console.log(results);
+		console.log(`${habitablePlanets.length} habitable planets`);
 		console.log('finished');
 	});
