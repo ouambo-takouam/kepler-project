295f2bdbb43b0924bde61168a05c0c47aae7d81b
patrick ouambo <patrickouambo@gmail.com>
2023-03-18 15:40:28 +0100

    fs readableStreams

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 
 const results = [];
 
-fs.createReadStream('./kepler_data.csv') // eventEmitter
-	.on('data', (chunk) => {
-		results.push(chunk);
-		console.log(chunk);
+fs.createReadStream('./kepler_data.csv')
+	.pipe(
+		parse({
+			comment: '#',
+			columns: true,
+		})
+	)
+	.on('data', (obj) => {
+		results.push(obj);
 	})
 	.on('error', (error) => console.log(error))
 	.on('end', () => {
+		console.log(results);
 		console.log('finished');
 	});
